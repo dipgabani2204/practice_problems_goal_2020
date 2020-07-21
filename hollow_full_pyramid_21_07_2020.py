566719378d40991c4462a7d6e4a69fb7dbb61c8d
Dipkumar Gabani <64963745+dipgabani2204@users.noreply.github.com>
2020-07-21 12:17:56 +0530

    Add files via upload

---
+++ hollow_full_pyramid_21_07_2020.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+	for j in range(1,14):
+		if j == 8-i or j ==6+i:
+			print('*',end="")
+		else:
+			print(' ',end="")
+	print('\n')
+for i in range(1,2):
+	for j in range(1,14):
+		if j == 2*i:
+			print('*',end="")
+			i = i+1;
+		else:
+			print(' ',end="")
